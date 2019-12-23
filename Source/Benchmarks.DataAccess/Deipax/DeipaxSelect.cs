9c3f3bbea056036c5fdd19a80a594e141668f316
Jeffery Felce <jfelce@hotmail.com>
2019-12-23 07:59:44 -0800

    Add Async benchmarks

--- Source/Benchmarks.DataAccess/Deipax/DeipaxSelect.cs
+++ Source/Benchmarks.DataAccess/Deipax/DeipaxSelect.cs
@@ -25,7 +25,7 @@ namespace Benchmarks.DataAccess.Deipax
         }
 
         [Benchmark]
-        public void AllFieldsAsClass_Deipax()
+        public void AllFieldsAsClass()
         {
             var tmp = _dbConnection
                 .CommandType(CommandType.Text)
@@ -34,9 +34,20 @@ namespace Benchmarks.DataAccess.Deipax
                 .ToList();
         }
 
+        [Benchmark]
+        public void AllFieldsAsClass_Async()
+        {
+            var tmp = _dbConnection
+                .CommandType(CommandType.Text)
+                .CommandText(_sql)
+                .AsEnumerableAsync<MultipleFieldClass>()
+                .Result
+                .ToList();
+        }
+
 
         [Benchmark]
-        public void AllFieldsAsStruct_Deipax()
+        public void AllFieldsAsStruct()
         {
             var tmp = _dbConnection
                 .CommandType(CommandType.Text)
@@ -46,12 +57,34 @@ namespace Benchmarks.DataAccess.Deipax
         }
 
         [Benchmark]
-        public void DynamicList_Deipax()
+        public void AllFieldsAsStruct_Async()
+        {
+            var tmp = _dbConnection
+                .CommandType(CommandType.Text)
+                .CommandText(_sql)
+                .AsEnumerableAsync<MultipleFieldStruct>()
+                .Result
+                .ToList();
+        }
+
+        [Benchmark]
+        public void DynamicList()
         {
             var tmp = _dbConnection
                 .CommandType(CommandType.Text)
                 .CommandText(_sql)
                 .AsEnumerable()
+                .ToList();
+        }
+
+        [Benchmark]
+        public void DynamicList_Async()
+        {
+            var tmp = _dbConnection
+                .CommandType(CommandType.Text)
+                .CommandText(_sql)
+                .AsEnumerableAsync()
+                .Result
                 .ToList();
         }
     }
